b70c0458f76f1f4337f5790d26e43ae3fdbd8cb1
Lucas julio <lucasjuliomts@gmail.com>
2022-12-02 00:29:44 -0300

    Commit entidades e seus construtores

---
+++ model/entities/Projetos.java
@@ -0,0 +1,76 @@
+public class Projetos {
+
+    private int id;
+    private String nome;
+    private String proposta;
+    private Cliente cliente;
+    
+    
+    
+    
+    public Projetos(int id, String nome, String proposta, Cliente cliente) {
+        this.id = id;
+        this.nome = nome;
+        this.proposta = proposta;
+        this.cliente = cliente;
+    }
+
+
+
+
+    public int getId() {
+        return id;
+    }
+
+
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+
+    public String getNome() {
+        return nome;
+    }
+
+
+
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+
+
+    public String getProposta() {
+        return proposta;
+    }
+
+
+
+
+    public void setProposta(String proposta) {
+        this.proposta = proposta;
+    }
+
+
+
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+
+
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    
+    
+}
