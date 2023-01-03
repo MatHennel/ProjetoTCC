dacdf590a632116e7701cb7feeb94affd3cda628
MatHennel <matheushennel123@gmail.com>
2023-01-02 23:50:11 -0300

    commit

--- deveconnection/src/main/java/br/com/deveconnection/model/DAO/DevDAO.java
+++ deveconnection/src/main/java/br/com/deveconnection/model/DAO/DevDAO.java
@@ -3,5 +3,6 @@ package br.com.deveconnection.model.DAO;
 import br.com.deveconnection.model.Entities.Dev;
 
 public interface DevDAO {
-    public Boolean cadastrarCliente(Dev dev);
+    public Boolean cadastrarCliente(Dev dev); 
+    
 }
